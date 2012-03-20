ddf44484f116222adae3d02dd579651ba6e1086c
David Stern <vorporeal@gmail.com>
2012-03-20 18:35:53 -0400

    Added base django project, and did some minor configuration.

---
+++ yipit_image_manipulation/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^yipit_image_manipulation/', include('yipit_image_manipulation.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
